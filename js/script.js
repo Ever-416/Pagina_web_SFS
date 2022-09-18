4d72bc7480ad2e65f10039e147a74697d249c5ea
EVER JESUS SALGADO GONZALEZ <ejsalgadog@ul.edu.co>
2022-09-17 20:40:59 -0500

    formulario contacto terminado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // funcionamiento carrusel
-
 const container = document.getElementById('carousel'),
     buttonLeft = document.getElementById('button-left'),
     buttonRight = document.getElementById('button-right');
@@ -35,27 +34,94 @@ buttonRight.addEventListener('click', () => {
 
 // funcionamiento enviar email
 
-const buttonSend = document.getElementById('sendEmail')
-buttonSend.addEventListener('click', (event)=> {
-    event.preventDefault()
-    let Name = document.getElementById('input-name').value,
-    Email = document.getElementById('input-email').value,
-    Phone = document.getElementById('input-tel').value,
-    Message = document.getElementById('input-menssage').value
-    Recaptcha = grecaptcha.getResponse();
-    
-    if (Name!='' & Email!='' & Phone!='' & Message!='' & Recaptcha !=0) {
+const formContact = document.getElementById('formContact'), 
+    Name = document.getElementById('input-name'),
+    Email = document.getElementById('input-email'),
+    Phone = document.getElementById('input-tel'),
+    Message = document.getElementById('input-menssage'), 
+    Warning = document.getElementById('formWarning');
+
+formContact.setAttribute('onsubmit', 'return sendForm();')
+
+function popup(action, funct){
+    let overlay = document.getElementById('overlay'),
+    items = document.querySelectorAll('#overlay .item');
+
+    if(action === 'hidden'){
+        overlay.classList.remove('active')
+    }else{
+        for (const element of items) {
+            element.style.setProperty('display', 'none');
+        }
+        items[funct].style.setProperty('display', 'flex');
+        overlay.classList.add('active');
+    }
+
+}
+
+const popupButtons = document.querySelectorAll('#overlay .item button')
+popupButtons[0].addEventListener('click', ()=> {popup('hidden', 0)});
+popupButtons[1].addEventListener('click', ()=> {popup('hidden', 0)});
+
+function clearForm (){
+    Name.value='', Email.value='', Phone.value='', Message.value='', grecaptcha.reset();
+    Warning.innerHTML = '';
+}
+
+function sendData (){
+    popup('', 0);
         axios.defaults.headers.post['Content-Type'] = 'application/json';
         axios.post('https://formsubmit.co/ajax/c649fc1ce0e6a95831be5af5e93d5957', {
-            nombre: Name,
-            telefono: Phone,
-            email: Email,
-            mensaje: Message,
+            nombre: Name.value.trim(),
+            telefono: Phone.value.trim(),
+            email: Email.value.trim(),
+            mensaje: Message.value.trim(),
             _subject: "Nuevo cliente",
-            _template: "table"
+            _template: "table",
+            _replyto: Email.value.trim()
         })
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            .then(response => {
+                if(response.status == 200){
+                    popup('hidden', 0);
+                    popup('', 1);
+                    // alert('Gracias por depositar su confianza en nosotros, lo estaremos contactando pronto');
+                    clearForm();
+                }
+            })
+            .catch(error => {
+                popup('hidden', 0);
+                popup('', 2);
+                // alert('Ocurrio un error al enviar el mensaje, por favor vuelva a intentarlo');
+                console.log(error);
+            });
+}
+
+function sendForm(){
+
+    let Recaptcha = grecaptcha.getResponse(),
+    acu='';
+
+    if (!/^[a-zA-Z ]{4,32}$/.test(Name.value.trim())){
+        acu+='Campo nombre no valido<br>';
+    }
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(Email.value)){
+        acu+='Campo Email no valido<br>';
+    }
+    if (!/^(3|\+57[ ]+3)([0-9]{6,6}|[0-9]{9,9})$/.test(Phone.value)){
+        acu+='Campo teléfono no valido<br>';
+    }
+    if (Message.value.trim().length<10 | Message.value.length>500){
+        acu+='Campo mensaje no valido<br>';
+    }
+    if (Recaptcha.length==0){
+        acu+='Complete el captcha <br>';
+    }
+
+    if(acu==''){
+       sendData();
+    }else{
+        Warning.innerHTML = acu;
     }
     
-});
+    return false;
+}
